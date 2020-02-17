3544d119e29b6dc4c95776273f2b83067777ace9
RMWalter <mrspuds559@gmail.com>
2020-02-17 15:55:42 -0700

    Trying linked-list tree data structure
    Went back and forth with making Nodes struct or classes and decided I'd rather they be classes so that I can restrict access to be only from within requisite nodes rather than having a big semi-global array of nodes to reference from that could also potentially be accessed separately outside of things like dialogue methods.
    
    I have a main() function for testing purposes at the moment since trying to import the fluent-asserts library to unit test one file instead of using Dub to build the entire project is tedious. Though I hven't run my own test code yet.
    
    Will probably be getting rid of the Tree class entirely since it's redundant with the Node structure I'm going for anyways.
    
    Will probably rename tree.d to something else but not sure what yet. Maybe something like logictree.d. I want to differentiate it from any inevitable tree.d file that has to do with defining actual in game trees. Like, the type with leaves and bird nests.

--- source/tree.d
+++ source/tree.d
@@ -1,3 +1,35 @@
+class Node
+{
+  /*
+  Nodes contain the text that is displayed on the screen for its selection, it's
+  resulting behaviour that runs as soon as it's chosen, and pointers to connected    nodes. For example, a node for a dialogue tree would contain the dialogue your
+  character says as its title, Its behaviour would be running any persuade or 
+  similar checks that need to be passed before moving on, as well as returning 
+  the dialogue the NPC says back to your character and any other resulting 
+  behaviour and the list of nodes for possible resulting dialogue choices. A 
+  combat action tree would list the title of the action, like "attack", "magic" 
+  or "item", which would link to / run the behaviour to be performed and/or a 
+  series of other nodes. Like the spells in a spell book or consumable items in 
+  inventory.
+  */
+     
+  auto head;
+  auto prev;
+  auto branch;
+
+  this()
+  {
+
+  }
+
+  hookup(Node H, Node P, Node[] B)
+  {
+    this.head = H;
+    this.prev = P;
+    this.branch = B;
+  }
+}
+
 class Tree
 {
   /*
@@ -9,29 +41,32 @@ class Tree
   up in their list of actions, which brings up a list of known spells when 
   selected. Whereas fighters have no such option.
   */
-
-  auto current; // where you currently are in the tree
 }
 
-class Node
+class Dialogue : Node
 {
-  /*
-  Nodes contain the text that is displayed on the screen for its selection, it's 
-  resulting behaviour that runs as soon as it's chosen, and pointers to connected 
-  nodes. For example, a node for a dialogue tree would contain the dialogue your 
-  character says as its title, Its behaviour would be running any persuade or 
-  similar checks that need to be passed before moving on, as well as returning 
-  the dialogue the NPC says back to your character and any other resulting 
-  behaviour and the list of nodes for possible resulting dialogue choices. A 
-  combat action tree would list the title of the action, like "attack", "magic" 
-  or "item", which would link to / run the behaviour to be performed and/or a 
-  series of other nodes. Like teh spells in a spell book or consumable items in 
-  inventory.
-  */
+  string message; //what is said when you get to this node
+  auto condition; //condition that needs to be met to progress to this node
+  string[] say; //What you can say
   
-  string[] titles;
-  delegate[] behaviour;
-  
-  Node next;
-  Node prev;
+  this(string M, auto C = null, string[] S)
+  {
+    super();
+    
+    this.message = M;
+    this.condition = C;
+    this.say = S;
+  }
+}
+
+void main()
+{
+  Node[] tree;
+  tree ~= Node("Hello, can I help you?", ["Yes", "No", "Bye"]);
+  tree ~= Node("With what?",["What\'s your Favorite colour?", "What\'s your name?", "Do you like bees?"]);
+  tree ~= Node("Oh, why are you here then?",["I'm bored", "Because why not?."]);  tree ~= Node("Green, I guess.",[]);
+  tree ~= Node("Emma.",[]);
+  tree ~= Node("I don't like things that sting me",[]);
+  tree ~= Node("Ummm... fair enough",[]);
+  tree ~= Node("",[]);
 }
