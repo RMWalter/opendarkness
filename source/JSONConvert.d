dae1ed50fe1c2442ff3e761fadaf389e13f77779
RMWalter <mrspuds559@gmail.com>
2020-03-23 19:22:54 -0600

    JSONConvert function fully working
    Will need to include more unit tests and do some refinement to make it portable but so far it appears to do what it's supposed to.

--- source/JSONConvert.d
+++ source/JSONConvert.d
@@ -2,81 +2,83 @@ version(unittest) import fluent.asserts;
 import std.json;
 import omni;
 
-union USON 
-{
-  ubyte[] UBYTEA;
-  int INT;
-  int[] INTA;
-  uint UINT;
-  uint[] UINTA;
-  string STRING;
-  string[] STRINGA;
-}
-
-omni JSONConvert(inout ref JSONValue value)
+omni JSONConvert(ref JSONValue value)
 {
   import std.conv : to;
 
-  omni temp;
-  switch(to!string(value.type))
+  static omni list(ref JSONValue value)
   {
-    case "array" :
     value = value.array;      
-      switch(to!string(value[0].type))
-      {
-        case "integer" :
-          foreach(i; value)
-          {
-            temp.INTA ~= to!int(i);
-          }
-         return temp;
-         break;
-        case "uinteger" :
-          foreach(i; value)
-          {
-            temp.UINTA ~= to!uint(i);
-          }
-          return temp;
-          break;
-        case "string" :
-          foreach(i; value)
-          {
-            temp.STRINGA ~= to!string(i);
-          }
-          return temp;
-          break;
-        default:
-          break;
-      }
-      break;
-    case "integer" :
-      value = value.integer;
-      temp.INT = to!int(value);
+    switch(to!string(value[0].type))
+    {
+      case "integer" :
+        int[] sub;
+        for(int i; i < value.array.length; i++)
+        {
+          sub ~= to!int(value[i].integer);
+        }
+      omni temp = {_INTA : sub};
       return temp;
       break;
-    case "uinteger" :
-      value = value.uinteger;
-      temp.UINT = to!uint(value);
-      return temp;
-      break;
-    case "string" :
-      value = value.string;
-      temp.STRING = to!string(value);
-      return temp;
-      break;
-    default:
-      break;
+      case "uinteger" :
+        uint[] sub;
+        for(int i; i < value.array.length; i++)
+        {
+          sub ~= to!uint(value[i].uinteger);
+        }
+        omni temp = {_UINTA : sub};
+        return temp;
+        break;
+      case "string" :
+        string[] sub;
+        for(int i; i < value.array.length; i++)
+        {
+          sub ~= to!string(value[i].str);
+        }
+        omni temp = {_STRINGA : sub};
+        return temp;
+        break;
+      default:
+        omni temp;
+        return temp;
+        break;
+    }
   }
-}
 
-void main()
-{
-  auto test = parseJSON(`{{"A" : "A", "B" : 1}}`);
-  string a = JSONConvert(test["A"]);
-  uint b = JSONConvert(test["B"]);
-  writeln(a);
-  writeln(b);
-
+  static omni single(ref JSONValue value)
+  {
+    switch(to!string(value.type))
+    {
+      case "integer" :
+        value = value.integer;
+        omni temp = {_INT : to!int(value.integer)};
+        return temp;
+        break;
+      case "uinteger" :
+        value = value.uinteger;
+        omni temp = {_UINT : to!uint(value.uinteger)};
+        return temp;
+        break;
+      case "string" :
+        value = value.str;
+        omni temp = {_STRING : to!string(value)};
+        return temp;
+        break;
+      default:
+        omni temp;
+        return temp;
+        break;
+    }
+  }
+  
+  if(to!string(value.type) == "array")
+  {
+    return list(value);
+  }
+  else
+  {
+    return single(value);
+  }
 }
 
 unittest
