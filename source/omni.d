d0e5a7165d7a24ab424a769435faf56999925846
RMWalter <mrspuds559@gmail.com>
2020-04-12 00:23:18 -0600

    Setting up get and set template methods for omni struct.

--- source/omni.d
+++ source/omni.d
@@ -1,58 +1,85 @@
 struct omni
 {
+  static union OMNI
+  {
+    ubyte[] UBYTEA;
+    byte[] BYTEA;
+    ushort[] USHORTA;
+    short[] SHORTA;
+    uint[] UINTA;
+    int[] INTA; 
+    float[] FLOATA;
+    ulong[] ULONGA;
+    long[] LONGA;
+    double[] DOUBLEA;
+    string[] STRINGA;
+    char[] CHARA;
+
+    ubyte UBYTE;
+    byte BYTE;
+    ushort USHORT;
+    short SHORT;
+    uint UINT;
+    int INT;
+    float FLOAT;
+    ulong ULONG;
+    long LONG;
+    double DOUBLE;
+    string STRING;
+    char CHAR;
+  }
+
   string TYPE;
-  omni VALUE;
-
-  void set()
+  OMNI VALUE;
+  static immutable string[string] TYPEDICT;
+  
+  void set(T)(in T input)
   {
-
+    TYPE = typeid(input);
+    VALUE.TYPEDICT[TYPE] = input;
   }
 
-  void get()
+  void get(T)(out T LVal)
   {
-
+    LVal = VALUE.TYPEDICT[TYPE];
   }
 }
 
-union omni
-{
-  ubyte[] UBYTEA;
-  byte[] BYTEA;
-  ushort[] USHORTA;
-  short[] SHORTA;
-  uint[] UINTA;
-  int[] INTA; 
-  float[] FLOATA;
-  ulong[] ULONGA;
-  long[] LONGA;
-  double[] DOUBLEA;
-  string[] STRINGA;
-  char[] CHARA;
-
-  ubyte UBYTE;
-  byte BYTE;
-  ushort USHORT;
-  short SHORT;
-  uint UINT;
-  int INT;
-  float FLOAT;
-  ulong ULONG;
-  long LONG;
-  double DOUBLE;
-  string STRING;
-  char CHAR;
-}
 /*
 void main()
 {
   import std.stdio : writeln;
 
   omni test;
+  test.TYPEDICT = ["ubyte[]" : "UBYTEA",
+  "byte[]" : "BYTEA",
+  "ushort[]" : "USHORTA",
+  "short[]" : "SHORTA",
+  "uint[]" : "UINTA",
+  "int[]" : "INTA",
+  "float[]" : "FLOATA",
+  "ulong[]" : "ULONGA",
+  "long[]" : "LONGA",
+  "double[]" : "DOUBLEA",
+  "string[]" : "STRINGA",
+  "char[]" : "CHARA",
+  "ubyte" : "UBYTE",
+  "byte" : "BYTE",
+  "ushort" : "USHORT",
+  "short" : "SHORT",
+  "uint" : "UINT",
+  "int" : "INT",
+  "float" : "FLOAT",
+  "ulong" : "ULONG",
+  "long" : "LONG",
+  "double" : "DOUBLE",
+  "string" : "STRING",
+  "char" : "CHAR"];
 
-  test.UBYTE = 98;
+  test.set = 98;
 
-  ubyte a = test.UBYTE;
-  char b = test.CHAR;
+  ubyte a = test.get;
+  char b = test.get;
   
   writeln(a);
   writeln(b);
