e381c6b952f7463749bbc8b7b1071367ce9ee85e
RMWalter <mrspuds559@gmail.com>
2020-04-22 00:35:14 -0600

    Added inventory framework to PartyManager class
    Added two nested static struct definitions and one placeholder delegate for handling "use" commands on items. Still working ou exactly what method I want to use to implement it all so this might change a few times. However I want t prioritize just getting something to work so I can push to my first minor release soon here and refine it all later.

--- source/party.d
+++ source/party.d
@@ -1,7 +1,24 @@
-class TBB_PartyManager
+class TBB_Party
 {
+  static struct TBB_Inventory
+  {
+    static struct ItemSlot
+    {
+      Item* Name; //points to an item definition
+      ubyte Amount;
+
+      delegate void Use()
+      {
+
+      }
+    }
+    ItemSlot[] Slot;
+  }
+  
   TBB_Hero[] Party;
 
+  TBB_Inventory Inventory; //holds the names of items and it's amount in each slot
+  
   this(TBB_Hero[] party)
   {
     this.Party = party;
