88e6e105df3178d25b89a6e86eba659d768b2529
RMWalter <mrspuds559@gmail.com>
2020-05-05 13:22:24 -0600

    Added TBB_Inventory to TBB_Party constructor

--- source/party.d
+++ source/party.d
@@ -6,8 +6,9 @@ class TBB_Party
 
   TBB_Inventory Inventory; //holds the names of items and it's amount in each slot
   
-  this(TBB_Hero[] party)
+  this(TBB_Hero[] party, TBB_Inventory inventory)
   {
     this.Party = party;
+    this.Inventory = inventory;
   }
 }
